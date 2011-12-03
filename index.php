6960c57da14c61e01fdbb3edbd7144fe55e58a13
Joe Cortopassi <joe@joecortopassi.com>
2011-12-02 20:36:32 -0800

    Added objects

--- index.php
+++ index.php
@@ -15,8 +15,28 @@
 	width: 10px;
 }
 
+.wall {
+	background-color: blue;
+	border: 1px black solid;
+	float:left;
+	height: 10px;
+	width: 10px;
+}
+
+.enemy {
+	background-color: red;
+	border: 1px black solid;
+	float:left;
+	height: 10px;
+	width: 10px;
+}
+
 .offMap {
-	display:none;
+	background-color: white;
+	border: 1px black solid;
+	float:left;
+	height: 10px;
+	width: 10px;
 }
 </style>
 
@@ -53,14 +73,104 @@ $map = rtrim( $map, ',' );
 $map .= "]";
 ?>
 
+<script>
+// Non-game related helper functions
+
+// 
+function cloneObject(obj) {
+    var clone = {};
+
+    for (var i in obj) {
+        if (typeof obj[i] == 'object') {
+            clone[i] = cloneObject(obj[i]);
+        } else {
+            clone[i] = obj[i];
+        }
+    }
+
+    return clone;
+}
+
+
+</script>
 
 <script>
 var player = { 'x' : 0, 'y' : 0 };
 var direction = 'none';
 var map = <?php echo $map; ?>;
-var view = { 'start': 0, 'end': 9 };
+var view = { 'start': 0, 'end': 9, 'buffer': 2};
+var object_template = { 'position' : { 'x' : 0, 'y': 0 }, 'velocity' : { 'x': 0, 'y': 0 }, 'vertices' : {}, 'type' : '' };
+var objects = [];
 
-window.onkeyup = function(e) {
+function setupObjects()
+{
+	var wall1 = cloneObject(object_template);
+	wall1.position = { 'x': 20, 'y': 0 };
+	wall1.vertices = [
+		{ 'x': 0, 'y': 0 },
+		{ 'x': 0, 'y': 1 },
+		{ 'x': 0, 'y': 2 },
+		{ 'x': 1, 'y': 0 },
+		{ 'x': 1, 'y': 1 },
+		{ 'x': 1, 'y': 2 }
+	];
+	wall1.type = 'wall';
+	objects.push(wall1);
+	
+	
+	var wall2 = cloneObject(object_template);
+	wall2.position = { 'x': 40, 'y': 0 };
+	wall2.vertices = [
+		{ 'x': 0, 'y': 0 },
+		{ 'x': 1, 'y': 0 },
+		{ 'x': 1, 'y': 1 },
+		{ 'x': 2, 'y': 0 },
+		{ 'x': 2, 'y': 1 },
+		{ 'x': 2, 'y': 2 },
+		{ 'x': 3, 'y': 0 },
+		{ 'x': 3, 'y': 1 },
+		{ 'x': 4, 'y': 0 }
+	];
+	wall2.type = 'wall';
+	objects.push(wall2);
+	
+	
+	var wall3 = cloneObject(object_template);
+	wall3.position = { 'x': 60, 'y': 0 };
+	wall3.vertices = [
+		{ 'x': 0, 'y': 0 },
+		{ 'x': 0, 'y': 1 },
+		{ 'x': 1, 'y': 0 },
+		{ 'x': 1, 'y': 1 },
+		{ 'x': 2, 'y': 0 },
+		{ 'x': 2, 'y': 1 }
+	];
+	wall3.type = 'wall';
+	objects.push(wall3);
+	
+	
+	var enemy1 = cloneObject(object_template);
+	enemy1.position = { 'x': 100, 'y': 0 };
+	enemy1.velocity = { 'x': -0.1, 'y': 0 };
+	enemy1.vertices = [
+		{ 'x': 0, 'y': 0 },
+		{ 'x': 0, 'y': 1 },
+		{ 'x': 1, 'y': 0 },
+		{ 'x': 1, 'y': 1 },
+		{ 'x': 2, 'y': 0 },
+		{ 'x': 2, 'y': 1 }
+	];
+	enemy1.type = 'enemy';
+	objects.push(enemy1);
+	
+	
+}
+
+
+
+
+// Set up movement to be based on key presses
+var keyDownEvent = function(e) {
 	if ( e.keyCode == 65 || e.keyCode == 37 ){
 		direction = 'left';
 	}
@@ -75,13 +185,15 @@ window.onkeyup = function(e) {
 	}
 }
 
+// Allow holding down of a key to be used for movement
+window.onkeydown = keyDownEvent;
+window.onkeypress = keyDownEvent;
+
+
+
 
 function updatePlayer()
-{
-	// Remove the old player marker
-	var old_cell = document.getElementById('cell_'+player.y+'_'+player.x);
-	old_cell.className = 'normal';
-	
+{	
 	// Update player position
 	if ( direction === 'left' )
 	{
@@ -114,24 +226,17 @@ function updatePlayer()
 	{
 		player.y = 0;	
 	}
-	else if ( player.x >= map[0].length )
+	else if ( player.x > view.end )
 	{
-		player.x = map[0].length - 1;	
+		player.x = view.end;	
 	}
-	else if ( player.x < 0 )
+	else if ( player.x < view.start )
 	{
-		player.x = 0;	
+		player.x = view.start;	
 	}
 	else
 	{
 		
-	}
-	
-	// Place the player back on the map
-	if ( player.x <= view.end && player.x >= view.start )
-	{
-		var new_cell = document.getElementById('cell_'+player.y+'_'+player.x)
-		new_cell.className = 'selected';
 	}
 	
 	direction = 'none';
@@ -139,10 +244,31 @@ function updatePlayer()
 
 
 
+function scrollViewWithPlayer()
+{
+	if ( ( player.x > view.end - view.buffer ) && player.x < map[0].length - view.buffer )
+	{
+		view.start++;
+		view.end++;
+	}
+	else if ( ( player.x < view.start + view.buffer ) && player.x >= 0 + view.buffer )
+	{
+		view.start--;
+		view.end--;
+	}
+	else
+	{
+		
+	}
+}
+
+
+
 function updateView()
 {
 	var cell;
-	// Loop for rows in map
+	
+	// Only show 
 	for ( var col = 0; col < map[0].length; col++ )
 	{
 		for ( var row = 0; row < map.length; row++ )
@@ -159,17 +285,87 @@ function updateView()
 			}
 		}
 	}
+	
+	// Place the player back on the map
+	if ( player.x <= view.end && player.x >= view.start )
+	{
+		var new_cell = document.getElementById('cell_'+player.y+'_'+player.x)
+		new_cell.className = 'selected';
+	}
 }
+
+
+function upateObjects()
+{
+	for( var i = 0; i < objects.length; i++ )
+	{
+		if ( objects[i].velocity.x != 0 )
+		{
+			objects[i].position.x += objects[i].velocity.x;
+		}
+		
+		if ( objects[i].velocity.y != 0 )
+		{
+			objects[i].position.y += objects[i].velocity.y;
+		}
+	}
+}
+
+function drawObjects()
+{
+	for( var i = 0; i < objects.length; i++ )
+	{
+		var object = objects[i];
+
+		for( var j = 0; j < object.vertices.length; j++ )
+		{
+			var vertice = object.vertices[j];
+
+			var verticePosition = { 'x': 0, 'y':0 }; 
+			
+			verticePosition.x = object.position.x + vertice.x;
+			verticePosition.y = object.position.y + vertice.y;
+			
+			if ( verticePosition.x <= view.end && verticePosition.x >= view.start )
+			{
+				drawObjectAtCoordinatesWithType( verticePosition.x, verticePosition.y, object.type );
+			}
+		}
+	}
+}
+
+
+function drawObjectAtCoordinatesWithType( x, y, type )
+{
+	x = Math.round(x);
+	y = Math.round(y);
+	
+	var cell = document.getElementById( 'cell_' + y + '_' + x );
+	
+	if ( cell !== null )
+	{
+		cell.className = type;
+	}
+}
+
+
+
+
 
 
 
 function gameLoop()
 {
-	updateView(); 
+	
 	updatePlayer();
+	upateObjects();
+	scrollViewWithPlayer();
+	updateView();
+	drawObjects();
 
-	setTimeout(gameLoop, 30);
+	setTimeout(gameLoop, 10);
 }
 
-setTimeout(gameLoop, 3000);
+setupObjects();
+setTimeout(gameLoop, 300);
 </script>
